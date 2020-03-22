114d55219c0b6d4645f3a94ff4a4a8667ab0d915
wealiar <wealiar@gmail.com>
2020-03-22 13:37:52 +0600

    deleted unused files and created 2 endpoint

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'test',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'test2',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.json(database.users);
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error loggin in');
+    }
+});
+
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    });
+
+    res.json(database.users[database.users.length - 1]);
+});
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT = user
+ */
