1ed41356e17ed14cf3192af57feb66cbc60e5860
wealiar <wealiar@gmail.com>
2020-05-24 23:10:45 +0600

    some changes during heroku deploying

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+const PORT_VAL = process.env.PORT || 3000
+app.listen(PORT_VAL, () => {
+    console.log(`app is running on port ${PORT_VAL}`);
 });
